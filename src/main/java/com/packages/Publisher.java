2c53a3d62d88d8e6e7dd1fe5bdb250310ef4af9d
Pouria Keshtidar <118597739+p2keshtidar@users.noreply.github.com>
2024-02-16 16:47:16 +0100

    Done

---
+++ src/main/java/com/packages/Publisher.java
@@ -0,0 +1,27 @@
+package com.packages;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Publisher {
+    private static final String EXCHANGE = "MyExchange";
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE, "fanout");
+            Scanner input = new Scanner(System.in);
+            String message;
+            do {
+                System.out.println("Enter message: ");
+                message = input.nextLine();
+                channel.basicPublish(EXCHANGE, "", null, message.getBytes());
+            } while (!message.equalsIgnoreCase("Quit"));
+        }
+    }
+}
